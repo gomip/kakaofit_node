2900471e8ee9721b84e3e3f1184a0f64dae8a193
김정훈 <junghoon15@rsquare.co.kr>
2021-02-15 17:24:55 +0900

    cors 수정

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ const app = express();
 
 // CORS Setting
 app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origins', ['http://localhost:3001', 'http://kakaofit.gomip.ml:3001']);
+  const allowed = ['http://localhost:3001', 'http://kakaofit.gomip.ml:3001'];
+  const origin = req.headers.origin;
+  if (allowed.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+  // res.header('Access-Control-Allow-Origin', ['http://localhost:3001', 'http://kakaofit.gomip.ml:3001']);
   res.header('Access-Control-Allow-Headers','X-Requested-With');
   next();
 });
